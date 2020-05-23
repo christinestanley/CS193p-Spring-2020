b3a5cca1949f80a04786e4657c82c7bedc8aef0c
Christine Stanley <chris@earlswoodmarketing.com>
2020-05-23 19:24:37 +0100

    Lecture 2 complete

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Chris on 23/05/2020.
+//  Copyright © 2020 Christine Stanley. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+        
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in emojis[pairIndex] }
+    }
+    
+    // MARK: - Access to the Model
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
